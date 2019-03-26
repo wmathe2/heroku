9cef14fb4d0c98068f0732908260a1da22dea794
wmathe2 <wmathe2@lsu.edu>
2019-03-26 12:48:24 -0500

    configuring for heroku

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Post
 
-# Create your views here.
+def post_list(request):
+    # This is a QuerySet that returns all the posts ordered by the publish date
+    posts = Post.objects.order_by('published_date')
+    # Pass the posts to the template
+    return render(request, 'blog/post_list.html', {'posts': posts})
---
+++ venv/lib/python3.7/hmac.py
@@ -0,0 +1,1 @@
+/Users/wmathews/anaconda3/lib/python3.7/hmac.py
